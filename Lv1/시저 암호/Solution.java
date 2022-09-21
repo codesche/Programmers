800c6ecdb67e7685718184b645eac5587d8bbeb3
Minsung <110509654+codesche@users.noreply.github.com>
2022-09-21 20:10:45 +0900

    Update Solution.java

--- Lv1/시저 암호/Solution.java
+++ Lv1/시저 암호/Solution.java
@@ -1,24 +1,20 @@
-class Solution {
+
+public class Solution {
     public String solution(String s, int n) {
         String answer = "";
-        
-        for (int i = 0; i < s.length() ; i++) {
-            char ch = s.charAt(i);
 
-            // 공백 처리
-            if (ch == ' ') {
-                answer += ch;
-                continue;
-            }
+        for (int i = 0; i < s.length() ; i++) {
+            char ch_arr = s.charAt(i);
 
             // 소문자 처리
-            if (ch >= 'a' && ch <= 'z') {
-                answer = ((ch + n) > 'z') ? answer + (char)(ch - 26 + n) : answer + (char)(ch + n);
-            } else if (ch >= 'A' && ch <= 'Z') {        // 대문자 처리
-                answer = ((ch + n) > 'Z') ? answer + (char)(ch - 26 + n) : answer + (char)(ch + n);
+            if (ch_arr >= 'a' && ch_arr <= 'z') {
+                answer = ((ch_arr + n) > 'z') ? answer + (char)(ch_arr - 26 + n) : answer + (char)(ch_arr + n);
+            } else if (ch_arr >= 'A' && ch_arr <= 'Z') {        // 대문자 처리
+                answer = ((ch_arr + n) > 'Z') ? answer + (char)(ch_arr - 26 + n) : answer + (char)(ch_arr + n);
+            } else if (ch_arr == ' ') {                         // 공백 처리
+                answer += ch_arr;
             }
         }
-        
         return answer;
     }
 }
